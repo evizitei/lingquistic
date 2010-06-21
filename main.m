e668519e0e0aa0df48e170ec83a926face1e0a12
evizitei <ethan.vizitei@gmail.com>
2010-06-21 17:10:25 -0500

    new xcode project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  lingquistic
+//
+//  Created by Ethan Vizitei on 6/21/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
